38a2bcae73aa7cac22b2f558e81b8a43e756594c
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-07-06 22:57:33 -0300

    adding basic functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  @ViewChild('mainCanvas')
+  public mainCanvas: ElementRef;
+
+  private context: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    this.context = this.mainCanvas.nativeElement.getContext('2d');
+  }
+
 }
