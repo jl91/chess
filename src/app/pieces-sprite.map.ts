58456f34744216770b3474ae7f4cd16a8b6cc20b
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-07-07 14:49:13 -0300

    adding pieces sprite map and pieces service

---
+++ src/app/pieces-sprite.map.ts
@@ -0,0 +1,15 @@
+export const PIECES_SPRITE_MAP = {
+    'whiteKing': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'whiteQueen': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'whiteBishop': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'whiteKnight': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'whiteRook': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'whitePawn': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackKing': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackQueen': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackBishop': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackKnight': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackRook': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+    'blackPawn': {'srcX': 0, 'srcY': 0, 'srcW': 0, 'srcH': 0, 'dstX': 0, 'dstY': 0, 'dstW': 0, 'dstH': 0},
+  }
+;
