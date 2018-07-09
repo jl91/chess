efd15bf811caaeb7e59f1e220afa3aedb2378b1d
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-07-09 00:05:49 -0300

    improving pieces-sprite.map.ts

---
+++ src/app/map/pieces-sprite.map.ts
@@ -0,0 +1,49 @@
+import { PieceSpriteCoordinateModel } from "../model/piece-sprite-coordinate.model";
+import { PiecesNamesEnum } from "../enum/pieces-names.enum";
+
+export class PiecesSpriteMap {
+  private map = new Map<PiecesNamesEnum, PieceSpriteCoordinateModel>();
+
+  constructor() {
+    this.map.set(PiecesNamesEnum.WHITE_KING, this.fabricateModel(0, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.WHITE_QUEEN, this.fabricateModel(45, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.WHITE_BISHOP, this.fabricateModel(90, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.WHITE_KNIGHT, this.fabricateModel(135, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.WHITE_ROOK, this.fabricateModel(179, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.WHITE_PAWN, this.fabricateModel(223, 0, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_KING, this.fabricateModel(0, 45, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_QUEEN, this.fabricateModel(45, 45, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_BISHOP, this.fabricateModel(90, 45, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_KNIGHT, this.fabricateModel(135, 45, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_ROOK, this.fabricateModel(179, 45, 48, 50, 0, 0, 75, 75));
+    this.map.set(PiecesNamesEnum.BLACK_PAWN, this.fabricateModel(223, 45, 48, 50, 0, 0, 75, 75));
+  }
+
+  public getPieceSpriteCoordinate(pieceName: PiecesNamesEnum): PieceSpriteCoordinateModel {
+    return this.map.get(pieceName);
+  }
+
+
+  private fabricateModel(
+    srcX: number,
+    srcY: number,
+    srcW: number,
+    srcH: number,
+    dstX: number,
+    dstY: number,
+    dstW: number,
+    dstH: number
+  ): PieceSpriteCoordinateModel {
+    const model = new PieceSpriteCoordinateModel();
+    model.srcX = srcX;
+    model.srcY = srcY;
+    model.srcW = srcW;
+    model.srcH = srcH;
+    model.dstX = dstX;
+    model.dstY = dstY;
+    model.dstW = dstW;
+    model.dstH = dstH;
+    return model;
+  }
+
+}
